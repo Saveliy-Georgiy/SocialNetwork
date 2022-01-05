69df0a49e8cc39ce00347d3304676eeb7f3c0e87
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2022-01-05 15:08:11 +0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import s from './Profile.module.css'
+
+const Profile = () => {
+    return (
+            <div className={s.content}><img
+                src="https://natworld.info/wp-content/uploads/2018/01/%D0%A1%D0%BE%D1%87%D0%B8%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-%D1%82%D0%B5%D0%BC%D1%83-%D0%9F%D1%80%D0%B8%D1%80%D0%BE%D0%B4%D0%B0.jpeg"
+                alt=""/>
+                <div>
+                    <div>ava +description</div>
+                    <div>
+                        my posts
+                        <div>
+                            new post
+                        </div>
+                        <div className={s.posts}>
+                        <div className={s.item}>post1</div>
+                        <div className={s.item}>post2</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    );
+};
+
+export default Profile;
