f30fb4a7ec34c4953a8d9ac06e4ffbc32d52993c
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2022-01-07 13:34:52 +0300

    Added component Dialogs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 
 const App = () => {
     return (
-        <div className="app-wrapper">
+        <div className="appWrapper">
             <Header/>
             <Navbar/>
-            <Profile/>
+               <div className="appWrapperContent">
+            <Dialogs/>
+           {/*  <Profile/>*/}
+            </div>
+
         </div>
     );
 }
