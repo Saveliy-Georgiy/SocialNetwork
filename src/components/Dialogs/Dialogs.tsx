6b143ed4923cce16c3b482b58c75abc92343c8ef
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2022-01-08 16:56:17 +0300

    Added styles for page Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,29 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Saveliy
+                </div>
+                <div className={s.dialog}>
+                    Dima
+                </div>
+                <div className={s.dialog}>
+                    Egor
+                </div>
+                <div className={s.dialog}>
+                    Artem
+                </div>
+                <div className={s.dialog}>
+                    Vano
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     );
 };
