14cd4b70c481d1440730207f423419b495e95ab4
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2022-02-28 12:42:18 +0300

    Added styles for user page

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,71 +5,73 @@ import {v1} from "uuid";
 
 const Users = (props: UsersPropsType) => {
 
-    if(props.usersPage.users.length === 0) {
-    props.setUsers([
-        {
-            id: v1(),
-            photoUrl: 'https://icon-library.com/images/avatar-icon/avatar-icon-6.jpg',
-            followed: true,
-            fullName: "Saveliy",
-            status: 'I am boss',
-            location: {city: "Minsk", country: "Belarus"}
-        },
-        {
-            id: v1(),
-            photoUrl: 'https://icon-library.com/images/avatar-icon/avatar-icon-6.jpg',
-            followed: false,
-            fullName: "Egor",
-            status: 'I am boss too',
-            location: {city: "Moskow", country: "Russia"}
-        },
-        {
-            id: v1(),
-            photoUrl: 'https://icon-library.com/images/avatar-icon/avatar-icon-6.jpg',
-            followed: false,
-            fullName: "Dima",
-            status: 'I am boss too',
-            location: {city: "Borisov", country: "Belarus"}
-        },
-        {
-            id: v1(),
-            photoUrl: 'https://icon-library.com/images/avatar-icon/avatar-icon-6.jpg',
-            followed: true,
-            fullName: "Vano",
-            status: 'I am boss too',
-            location: {city: "Grodno", country: "Belarus"}
-        },])
+    if (props.usersPage.users.length === 0) {
+        props.setUsers([
+            {
+                id: v1(),
+                photoUrl: 'https://icon-library.com/images/avatar-icon/avatar-icon-6.jpg',
+                followed: true,
+                fullName: "Saveliy",
+                status: 'I am boss',
+                location: {city: "Minsk", country: "Belarus"}
+            },
+            {
+                id: v1(),
+                photoUrl: 'https://icon-library.com/images/avatar-icon/avatar-icon-6.jpg',
+                followed: false,
+                fullName: "Egor",
+                status: 'I am boss too',
+                location: {city: "Moskow", country: "Russia"}
+            },
+            {
+                id: v1(),
+                photoUrl: 'https://icon-library.com/images/avatar-icon/avatar-icon-6.jpg',
+                followed: false,
+                fullName: "Dima",
+                status: 'I am boss too',
+                location: {city: "Borisov", country: "Belarus"}
+            },
+            {
+                id: v1(),
+                photoUrl: 'https://icon-library.com/images/avatar-icon/avatar-icon-6.jpg',
+                followed: true,
+                fullName: "Vano",
+                status: 'I am boss too',
+                location: {city: "Grodno", country: "Belarus"}
+            },])
     }
     return (
         <div>
             {
-                props.usersPage.users.map(u => <div key={u.id}>
-                <span>
-                <div>
-                    <img className={s.photo} src={'https://icon-library.com/images/avatar-icon/avatar-icon-6.jpg'} alt="photo"/>
-                </div>
-                <div>
-                    {u.followed ?
-                        <button onClick={() => {
-                            props.follow(u.id)
-                        }}>Follow</button> :
-                        <button onClick={() => {
-                            props.unfollow(u.id)
-                        }}>Unfollow</button>}
+                props.usersPage.users.map(u =>
+                    <div key={u.id} className={s.userWrapper}>
+                        <div className={s.imgWrapper}>
+                            <div>
+                                <img className={s.photo}
+                                     src={'https://icon-library.com/images/avatar-icon/avatar-icon-6.jpg'} alt="photo"/>
+                            </div>
+                            <div className={s.buttonWrapper}>
+                                {u.followed ?
+                                    <button onClick={() => {
+                                        props.follow(u.id)
+                                    }}>Follow</button> :
+                                    <button onClick={() => {
+                                        props.unfollow(u.id)
+                                    }}>Unfollow</button>}
 
-                </div>
-                    </span>
-                    <span>
-                    <span>
-                        <div>{u.fullName}</div>
-                        <div>{u.status}</div>
-                    </span>
-                        <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
-                    </span>
-                    </span>
-                </div>)
+                            </div>
+                        </div>
+                        <div className={s.informationWrapper}>
+                            <div className={s.message}>
+                                <div className={s.name}>{u.fullName}</div>
+                                <div>{u.status}</div>
+                            </div>
+                            <div className={s.locationWrapper}>
+                                <div className={s.location}>{u.location.country},</div>
+                                <div className={s.location}>{u.location.city}</div>
+                            </div>
+                        </div>
+                    </div>)
             }
         </div>
     );
