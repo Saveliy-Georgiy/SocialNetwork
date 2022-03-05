2c20920e8c0d76748ad39afc4d986726f07604be
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2022-03-05 16:33:15 +0300

    ctrl alt l

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -4,14 +4,14 @@ import s from "./Users.module.css";
 import axios from "axios";
 
 class Users extends React.Component<UsersPropsType, {}> {
-constructor(props: UsersPropsType) {
-    super(props);
-    if (props.usersPage.users.length === 0) {
-    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-        this.props.setUsers(response.data.items)
-})
-}
-}
+    constructor(props: UsersPropsType) {
+        super(props);
+        if (props.usersPage.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                this.props.setUsers(response.data.items)
+            })
+        }
+    }
 
     render() {
         return (
@@ -22,7 +22,8 @@ constructor(props: UsersPropsType) {
                             <div className={s.imgWrapper}>
                                 <div>
                                     <img className={s.photo}
-                                         src={u.photos.small !== null ? u.photos.small :'https://icon-library.com/images/avatar-icon/avatar-icon-6.jpg'} alt="photo"/>
+                                         src={u.photos.small !== null ? u.photos.small : 'https://icon-library.com/images/avatar-icon/avatar-icon-6.jpg'}
+                                         alt="photo"/>
                                 </div>
                                 <div className={s.buttonWrapper}>
                                     {u.followed ?
