2c277d532472ae7b16b086b7cba71dbc2c102446
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2022-03-25 15:53:43 +0300

    Refactored tests

--- src/redux/dialogsReducer.test.ts
+++ src/redux/dialogsReducer.test.ts
@@ -1,33 +1,10 @@
 import {v1} from "uuid";
 import dialogsReducer, {DialogPageType, SEND_MESSAGE, UPDATE_NEW_MESSAGE_BODY} from "./dialogsReducer";
-const initialState: DialogPageType = {
-    dialogs: [
-        {id: v1(), name: "Saveliy"},
-        {id: v1(), name: "Dima"},
-        {id: v1(), name: "Egor"},
-        {id: v1(), name: "Artem"},
-        {id: v1(), name: "Vano"},
-    ],
-    messages: [
-        {id: v1(), message: "Yo"},
-        {id: v1(), message: "How are you?"},
-        {id: v1(), message: "Hello"},
-        {id: v1(), message: "Nice project"},
-        {id: v1(), message: "What am I doing here?"},
-    ],
-    newMessageBody: ""
-}
 
-test("reducer should be add update new message", () => {
+let initialState: DialogPageType
 
-
-    const newState = dialogsReducer(initialState, {type: UPDATE_NEW_MESSAGE_BODY, body: "some text"})
-
-    expect(newState.newMessageBody).toBe("some text")
-})
-
-test("reducer should send message", () => {
-    const initialState: DialogPageType = {
+beforeEach(() => {
+    initialState = {
         dialogs: [
             {id: v1(), name: "Saveliy"},
             {id: v1(), name: "Dima"},
@@ -44,6 +21,17 @@ test("reducer should send message", () => {
         ],
         newMessageBody: ""
     }
+})
+
+test("reducer should be add update new message", () => {
+
+
+    const newState = dialogsReducer(initialState, {type: UPDATE_NEW_MESSAGE_BODY, body: "some text"})
+
+    expect(newState.newMessageBody).toBe("some text")
+})
+
+test("reducer should send message", () => {
 
     const newState = dialogsReducer(initialState, {type: SEND_MESSAGE})
 
