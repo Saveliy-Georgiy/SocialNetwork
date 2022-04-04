b5a71dfba8162ec3aaa113bffb93bcb40e68d329
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2022-04-04 12:57:15 +0300

    deleted debugger

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,6 @@ import MyPostsContainer from "./MyPosts/MyPostsContainer";
 import {ProfilePropsType} from "./ProfileContainer";
 
 const Profile = (props: ProfilePropsType) => {
-    debugger
     return (
         <div>
             <ProfileInfo profile={props.profile} status={props.status} updateStatus={props.updateStatus}/>
